fff3d8e5a5914c3e3800c3031db1479b883a1829
Lonnie <wibberding@gmail.com>
2015-06-08 10:56:26 -0700

    Next section 7.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger  Use to debug in the server command prompt
+  end
 end
