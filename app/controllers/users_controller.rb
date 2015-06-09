68a6707ebde656bc712aee26d20248f86e91b993
Lonnie <wibberding@gmail.com>
2015-06-09 09:58:32 -0700

    Next section 7.4.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
     # debugger  Use to debug in the server command prompt
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+
+
 end
