3a4ab41f0d8ce8a3c119beec83852cff3edc5852
Lonnie <wibberding@gmail.com>
2015-06-11 09:02:31 -0700

    Finish log in/log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user) #Takes input from remember me checkbox.
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
