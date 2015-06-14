27fad52baf0eab88de5a4a38ac591d4500a0d117
Lonnie <wibberding@gmail.com>
2015-06-14 12:11:09 -0700

    Partially through 10.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post 'login'    =>  'sessions#create'
   delete 'logout' =>  'sessions#destroy'
   resources :users
+  resources :acount_activations, only: [:edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
